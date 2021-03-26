cdefb002a59b58af209f830351dca9eb7f06b4e0
Leonidas Peligrineli <l.peligrineli@comunique-se.com.br>
2021-03-26 10:47:17 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+var $simpleCarousel = document.querySelector(".js-carousel--simple");
+
+new Glider($simpleCarousel, {
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  draggable: true,
+  dots: ".js-carousel--simple-dots",
+  arrows: {
+    prev: ".js-carousel--simple-prev",
+    next: ".js-carousel--simple-next",
+  },
+});
+var $simpleCarousel = document.querySelector(".js-carousel--simple");
+ 
+new Glider($simpleCarousel, {
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  draggable: true,
+  dots: ".js-carousel--simple-dots",
+  arrows: {
+    prev: ".js-carousel--simple-prev",
+    next: ".js-carousel--simple-next",
+  },
+});
